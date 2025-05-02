4f52ee1728097183d864f05be01a1b08e760ebec
dodu232 <dodu7510@gmail.com>
2025-05-02 17:01:09 +0900

    refactor: User에 nickcname 추가

--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -7,9 +7,11 @@ public class UserResponse {
 
     private final Long id;
     private final String email;
+    private final String nickname;
 
-    public UserResponse(Long id, String email) {
+    public UserResponse(Long id, String nickname, String email) {
         this.id = id;
+        this.nickname = nickname;
         this.email = email;
     }
 }
