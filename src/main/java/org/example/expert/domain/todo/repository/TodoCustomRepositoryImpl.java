357187f813a3903574954b179e239b9f5a72b5a9
dodu232 <dodu7510@gmail.com>
2025-05-07 18:07:45 +0900

    refactor: querydsl 사용하도록 수정

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepositoryImpl.java
@@ -0,0 +1,32 @@
+package org.example.expert.domain.todo.repository;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.common.exception.InvalidRequestException;
+import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class TodoCustomRepositoryImpl implements TodoCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Todo findByIdWithUser(Long id) {
+        Todo result = jpaQueryFactory.selectFrom(todo)
+            .where(todo.id.eq(id))
+            .leftJoin(todo.user)
+            .fetchJoin()
+            .fetchOne();
+
+        if (result == null){
+            throw new EntityNotFoundException("Todo not found");
+        }
+
+        return result;
+    }
+}
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
@@ -24,10 +24,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u WHERE t.weather LIKE :weather AND t.modifiedAt BETWEEN :startDate AND :endDate ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByWeatherAndDateRange(@Param("weather") String weather, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Pageable pageable);
-
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
